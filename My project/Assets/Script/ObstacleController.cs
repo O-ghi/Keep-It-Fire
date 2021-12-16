c3afa0b5c34cf2684526bbfa01ff17adcdabb2ff
Khang <chikhang4566@gmail.com>
2021-12-17 02:44:57 +0700

    ADD - add controller for BigObstacle

--- My project/Assets/Script/ObstacleController.cs
+++ My project/Assets/Script/ObstacleController.cs
@@ -4,27 +4,42 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
+    //public parameter
+    public float destroyTime = 1f;
     Transform randomPointPosition;
     bool isCollision = false;
-
     private void Start()
     {
         GameObject randomPoint = GameObject.Find("ObstaclePoint");
         randomPointPosition = randomPoint.transform;
-        Debug.Log(randomPointPosition);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag.Equals("Obstacle") && isCollision == false && this.gameObject.transform.position.y > randomPointPosition.position.y)
         {
-            this.gameObject.transform.position += new Vector3(0.5f, 0f, 0f);
+            this.gameObject.transform.position += new Vector3(0.5f, 0f, 0f); //Obstacle will push the other if they touch each other
             isCollision = true;
         }
 
         if (other.gameObject.tag.Equals("Ground"))
         {
-            Destroy(this.gameObject);
+            if (this.gameObject.tag.Equals("BigObstacle")) //BigObstacle will coutdown to destroy
+            {
+                StartCoroutine(DestroyAfter(destroyTime));
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+
         }
+    }
+
+    IEnumerator DestroyAfter(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Debug.Log("Destroy");
+        Destroy(this.gameObject);
     }
 }
