3877efb39a68f8a77f27b9bb2abaea03da1a1518
Khang <chikhang4566@gmail.com>
2021-12-28 20:59:12 +0700

    ADD - add randomBigObstacle function, Increase total Obstacle time by time

--- My project/Assets/Script/RandomObstacleController.cs
+++ My project/Assets/Script/RandomObstacleController.cs
@@ -1,30 +1,34 @@
 using UnityEngine.UI;
 using UnityEngine;
 
+
 public class RandomObstacleController : MonoBehaviour
 {
     //public parameter 
     public GameObject obstacle;
     public GameObject bigObstacle;
-
+    public GameObject warningIcon;
 
     //public value
-    public float timeCouting = 1;
-    public int scoreAppear = 20;
+    public float timeCouting = 1f;
+    public int scoreToAppear = 10;
+    public int scoreToInscrease = 20;
     public int totalObstaclePerTurn = 10;
 
     //parameter
     GameObject scoreText;
-
+    GameObject player;
     //value
 
     bool isCreated = false;
+    bool isIncreased = false;
+    int increaseValue = 0;
 
 
     void Start()
     {
         scoreText = GameObject.FindGameObjectWithTag("ScoreText");
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -33,7 +37,7 @@ public class RandomObstacleController : MonoBehaviour
 
         int score = int.Parse(scoreText.GetComponent<Text>().text);
 
-        if (score % scoreAppear == 0 && score > 0)
+        if (score % scoreToAppear == 0 && score > 0)
         {
             if (isCreated == false)
             {
@@ -46,8 +50,6 @@ public class RandomObstacleController : MonoBehaviour
             isCreated = false;
         }
 
-        Debug.Log(isCreated);
-
         if (timeCouting <= 0)
         {
             for (int i = 0; i < totalObstaclePerTurn; i++)
@@ -55,6 +57,26 @@ public class RandomObstacleController : MonoBehaviour
                 randomObstacle();
             }
             timeCouting = 1;
+        }
+
+        //Increase total Obstacle time by time
+        if (score % scoreToInscrease == 0 && score > 0)
+        {
+            if (isIncreased == false)
+            {
+                timeCouting -= 0.1f;
+                totalObstaclePerTurn += 1;
+                if (score % 25 == 0 && scoreToAppear >= 5)
+                {
+                    scoreToAppear -= 1;
+                    increaseValue++;
+                }
+            }
+            isIncreased = true;
+        }
+        else
+        {
+            isIncreased = false;
         }
 
         timeCouting -= Time.deltaTime;
@@ -70,7 +92,13 @@ public class RandomObstacleController : MonoBehaviour
 
     void randomBigObstacle()
     {
-        float randomPositionX = Random.Range(-10f, 10f);
-        Instantiate(bigObstacle, new Vector3(randomPositionX, this.transform.position.y + 5f), Quaternion.identity);
+        float randomPositionX = Random.Range(player.transform.position.x + 3f, player.transform.position.x - 3f);
+        Instantiate(bigObstacle, new Vector3(randomPositionX, this.transform.position.y + 10f, -1f), Quaternion.identity);
+        Instantiate(warningIcon, new Vector3(randomPositionX, 4.4f, -1f), Quaternion.identity);
+    }
+
+    public int getIncreaseValue()
+    {
+        return this.increaseValue;
     }
 }
